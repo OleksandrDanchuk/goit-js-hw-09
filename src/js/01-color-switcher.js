df825524ee00593412d22e8e9a80ba04678bd201
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-04-21 22:37:21 +0300

    1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+let intervalId;
+
+function startColorSwitch() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopColorSwitch() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', startColorSwitch);
+stopButton.addEventListener('click', stopColorSwitch);
