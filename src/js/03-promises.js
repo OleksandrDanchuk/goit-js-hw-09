b4ff1b91a68cfdeebba819fb294b2c48949d9c2e
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-04-22 18:12:50 +0300

    3task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  if (step <= 0 || delay <= 0 || amount <= 0) {
+    Notiflix.Notify.failure(
+      'Please enter positive values for delay, step, and amount'
+    );
+    return;
+  }
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
